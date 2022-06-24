312f38039c34c4046f6464c73c6233c97604b7f6
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-24 16:29:52 +0600

    connected to MongoDB

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const app = express()
 const port = process.env.port || 5500
 
 // Middleware
-app.use(express.json()) 
+app.use(express.json())
 const cors = require('cors')
-app.use(cors()) 
+app.use(cors())
 
 // dot env
 require('dotenv').config()
-console.log(process.env.DB_PASSWORD);
+
+// mongo connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tcqve.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function connect() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err.codeName,`& ${err.message}`);
+    }
+
+
+
+}
+connect().catch(console.dir);
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
