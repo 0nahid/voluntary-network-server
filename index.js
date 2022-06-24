2b2d0385df3a94a30ebb87c75774c9fa3f5e1521
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-24 16:23:06 +0600

    require dot env

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors()) 
 
+// dot env
+require('dotenv').config()
+console.log(process.env.DB_PASSWORD);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
